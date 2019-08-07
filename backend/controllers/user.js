44eae67e38647de36562f144dd3a04a9289ca56b
Alex <alex11535@bezeqint.net>
2019-08-07 04:24:50 +0400

    add controllers

---
+++ backend/controllers/user.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+
+exports.createUser = (req, res, next) => { //Dvir it's export-s-! and not export
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Invalid authentication credentials!"
+        });
+      });
+  });
+};
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign(
+        {email: fetchedUser.email, userId: fetchedUser._id },
+        "alex_and_dvir_secret",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+        userId: fetchedUser._id
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    });
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,69 +1,12 @@
 const express = require("express");
-const bcrypt = require('bcrypt');
-const User = require("../models/userModel");
-const jwt = require("jsonwebtoken");
-
+const UserController = require("../controllers/user");
 const router = express.Router();
 
 
-router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    const user = new User({
-      email: req.body.email,
-      password: hash
-    });
-    user
-      .save()
-      .then(result => {
-        res.status(201).json({
-          message: "User created!",
-          result: result
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: "Invalid authentication credentials!"
-        });
-      });
-  });
-});
+router.post("/signup", UserController.createUser); // Dvir you pass here the reference and not function
 
 
-router.post("/login", (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Auth failed"
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(401).json({
-          message: "Auth failed"
-        });
-      }
-      const token = jwt.sign(
-        {email: fetchedUser.email, userId: fetchedUser._id },
-        "alex_and_dvir_secret",
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600,
-        userId: fetchedUser._id
-      });
-    })
-    .catch(err => {
-      return res.status(401).json({
-        message: "Auth failed"
-      });
-    });
-});
+router.post("/login", UserController.userLogin); // the same here
 
 
 module.exports = router;
