a09a188c1098e2678066def29b7d9987c399d0a6
Alex <alex1mosh@gmail.com>
2019-08-10 01:34:24 +0300

    final oral fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import {PostListComponent} from './posts/post-list/Post-list.component';
 import {AuthGuard} from './auth/auth.guard';
-import {MapComponent} from './map/map.component';
 import {UserProfileComponent} from './user-profile/profile-info/user.profile.component';
 import {MapComponent} from './map/map-google/map.component';
 
@@ -11,9 +10,6 @@ const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent , canActivate: [AuthGuard]},
   { path: 'edit/:postId', component: PostCreateComponent , canActivate: [AuthGuard]},
-  { path: 'login', component: LoginComponent},
-  { path: 'signup', component: SignupComponent},
-  {path: 'map' , component: MapComponent}
   { path: 'userprofile', component: UserProfileComponent , canActivate: [AuthGuard]},
   { path: 'map', component: MapComponent , canActivate: [AuthGuard]},
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
